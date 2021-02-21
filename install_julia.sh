990b9d5eaccc9a4b82df2a976dff598233a0e749
Asapanna Rakesh <45640029+rakesh4real@users.noreply.github.com>
2021-02-21 22:51:09 +0000

    install julia

---
+++ install_julia.sh
@@ -0,0 +1,3 @@
+wget https://julialang-s3.julialang.org/bin/linux/x64/1.5/julia-1.5.3-linux-x86_64.tar.gz
+tar zxvf julia-1.5.3-linux-x86_64.tar.gz
+export PATH="$PATH:/home/codespace/julia-1.5.3/bin"
